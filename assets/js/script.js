805c10112e66ada06e08f0013ed5c82a985665c7
Henrique Pesciotti <hpesciotti@gmail.com>
2024-04-04 20:24:50 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,43 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elemet and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
+    
     for (let button of buttons) {
-        button.addEventListener("click", function () {
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             }
             else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You cliked ${gameType}`);
+                runGame(gameType);
             }
-        })
+        });
     }
-})
+
+    runGame("addition");
+
+});
+
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
- */  
-function runGame() {
-    let num1 = Math.floor(Math.random() *25) + 1;
-    let num2 = Math.floor(Math.random() *25) + 1;
+ */
+function runGame(gameType) {
+    
+    //Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unkown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {
@@ -41,7 +56,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
