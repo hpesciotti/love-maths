bae87fa054a6195c67784008e5cb3f9de30d2311
Henrique Pesciotti <hpesciotti@gmail.com>
2024-04-05 18:36:00 +0000

    Add subtract game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unkown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -76,8 +78,10 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
-        return [operand1 * operand2, "multiply"]
-    } else {
+        return [operand1 * operand2, "multiply"];
+    } else if(operator === "-"){
+        return [operand1 - operand2, "subtract"];
+    }else {
         alert(`Unimplemented operator ${operator}`);
         thorw`Unimplemented operator ${operator}. Aborting!`;
     }
@@ -112,7 +116,11 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-"    
 
 }
 
